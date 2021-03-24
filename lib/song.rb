a68cbc7c2eadb994e086a03182a60e5f2a20c3b8
Jacob W Jones <jacobwadejones76@gmail.com>
2021-03-24 15:54:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,10 +14,38 @@ class Song
   
   def self.new_by_name(song_name)
     song = self.new
-    song.name = 
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    self.all.find do |songs|
+      songs.name == song_name
+    end
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by do |songs|
+      songs.name
+    end
+  end
   
   def save
     self.class.all << self
+  end
+  
+  def self.destroy_all
+    self.all.clear
   end
   
 end
